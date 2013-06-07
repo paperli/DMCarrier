402e6e3cfb57e9898293aac3a4b24f7bfb5ab397
paperli <paper.li@gmail.com>
2013-06-07 11:24:14 +0800

    Major update

--- www/js/index.js
+++ www/js/index.js
@@ -17,71 +17,6 @@
  * under the License.
  */
 
-function gmapInit(){
-    var mapOptions = {
-        zoom: 4,
-        center: new google.maps.LatLng(-33, 151),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-    
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-    return map;
-}
-
-//var map;
-var MYAPP = MYAPP || {};
-
-MYAPP.run = function(){
-    //var map = gmapInit();
-    
-    MYAPP.app = new kendo.mobile.Application(document.body, { transition: "slide" });
-    $('#map-canvas').height($('#map-canvas').parents('div[data-role=content]').height());
-    navigator.geolocation.getCurrentPosition(onSuccess,onError);
-};
-
-function onError(error){
-    alert('code: ' + error.code + '\n' +
-          'message: ' + error.message + '\n');
-}
-
-function onSuccess(position){
-    alert("get position success! Lat:" + position.coords.latitude + " Lng:" + position.coords.longitude);
-    //gmapInit();
-    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    var opts = {
-        zoom:8,
-        center: latlng,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'), opts);
-    
-}
-
-MYAPP.reddit = kendo.data.DataSource.create({
-                                            transport:{
-                                                read: "http://www.reddit.com/r/programming.json"
-                                            },
-                                            schema: {
-                                                data:"data.children",
-                                                fields: {
-                                            title:"data.title"
-                                                }
-                                            }
-});
-
-
-
-(function(){
- 
- if (navigator.userAgent.indexOf('Browzr') > -1){
-    // blackberry
-    setTimeout(MYAPP.run,250);
- } else {
-    //document.addEventListener('deviceready',onDeviceReady, false);
-    document.addEventListener('deviceready',MYAPP.run, false);
- }
-})();
-
 var app = {
     // Application Constructor
     initialize: function() {
@@ -113,3 +48,17 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
+
+var mapOptions;
+
+$(function(){
+  app.initialize();
+  mapOptions = {
+  //buttonCallback: "cbMapCallback",
+  height: $(window).height() - $("[data-role=footer]").height() - $("[data-role=header]").height() - 20,
+  diameter: 1000,
+  offsetTop: $("div[data-role=header]").height(),
+  lat: pins[0].lat,
+  lon: pins[0].lon
+  };
+})();
